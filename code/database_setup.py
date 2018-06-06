981fd22ff69aa4ced6b21a4156a2d385a2c6a0a2
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-06-05 22:03:14 -0700

    added prep files for project code

---
+++ code/database_setup.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Sequence
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
+# from sqlalchemy import create_engine
+ 
+# Base = declarative_base()
+
+# Add Categories and Items, along with user storage space
+
+# engine = create_engine('sqlite:///useritemcatalog.db')
+# Base.metadata.create_all(engine)
