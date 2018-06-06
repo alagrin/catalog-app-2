4d01ceb201489927ddbe9737e0944ba0c96b82f0
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-06-06 16:34:25 -0700

    added database table file

---
+++ catalog/models.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, \
+login_required, RoleMixin
+
+# Creates app
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'my-super-secret-key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///itemcatalog.db'
+
+# Creates Database connection object
+db = SQLAlchemy(app)
+
+#Definition of models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(30), unique=True, nullable=False)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    category = db.Column(db.String(120), nullable=False)
+    added_at = db.Column(db.String(120), nullable=False)
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Item)
+security = Security(app, user_datastore)
+
+# if __name__ == '__main__':
+# db.create_all(app)
+#     app.run()
