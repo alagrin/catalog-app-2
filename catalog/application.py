92effdbcbf6a3382af697187320dcf03693b79f7
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-07-06 13:25:04 -0700

    added google signin option, helper function file, prep for login/logout functionality

--- catalog/application.py
+++ catalog/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, \
 jsonify, session as login_session, make_response, abort
-from helpers import filterByCategory
+from helpers import gen_random_string
 import random, string, httplib2, json, requests, os
 from db_setup import Base, User, Item
 from sqlalchemy import create_engine, desc
@@ -29,9 +29,9 @@ def main():
 @app.route('/login', methods=['GET','POST'])
 def login():
     # session.rollback()
-    # state = ''.join(random.choice(
-    #     string.ascii_uppercase + string.digits) for x in xrange(32))
-    # login_session['state'] = state    Will add STATE=state as well
+    valid = (string.ascii_letters, string.digits, ':*&^') # for google login
+    state = gen_random_string(valid, 32) # set state to the result of the func above
+    login_session['state'] = state # passing value of state to the login_session- preventing CSF attacks
 
     if request.method == 'POST':
         newUser = User(email=request.form['email'], \
@@ -46,7 +46,7 @@ def login():
         print("user added")
         return redirect(url_for('catalogHome')) # will contain user_email context
     else:
-        return render_template('login.html')
+        return render_template('login.html', STATE=state)
 
 @app.route('/logout')
 def logout():
@@ -55,7 +55,7 @@ def logout():
 
 @app.route('/catalog/', methods = ['GET', 'POST'])
 def catalogHome():
-    categories = ['home', 'sports', 'clothing', 'business', 'personal']
+    categories = ['home', 'sports', 'clothing', 'business', 'personal'] #adjust this/ clean up
     allItems = session.query(Item).order_by(desc(Item.added_at)).limit(20).all()
     totalItems = session.query(Item).count()
     # TODO: add categories rep to loop through and show active categories
@@ -73,7 +73,6 @@ def categoryItems(category):
 
 @app.route('/catalog/<category>/<int:item_id>')
 def itemInfo(category, item_id):
-    # TODO: Suddenly item info won't work, item found but template doesn't load
     try:
         item = session.query(Item).filter_by(id=item_id).one()
         if item:
@@ -96,7 +95,6 @@ def newItem():
         return render_template('newitem.html')
 
 @app.route('/catalog/<category>/<int:item_id>/edit', methods=['GET', 'POST'])
-# @login_required
 def editItem(category, item_id):
     itemToEdit = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
@@ -115,7 +113,6 @@ def editItem(category, item_id):
         item_id=item_id, category=category)
 
 @app.route('/catalog/<category>/<item_id>/delete', methods=['GET', 'POST'])
-# @login_required
 def deleteItem(category, item_id):
     itemToDelete = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
@@ -131,14 +128,109 @@ def jsonCatalog():
     items = session.query(Item).all()
     return jsonify(Items=[i.serialize for i in items])
 
-# @app.route('/gconnect', methods=['POST'])
-# def gconnect():
-#     if request.args.get('state') != login_session['state']:
-#         response = make_response(json.dumps('Invalid state parameter.'), 401)
-#         response.headers['Content-Type'] = 'application/json'
-#         return response
-#     access_token = request.data
-#     print("access token received as {}".format(access_token))
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    authCode = request.data
+    print("access token received as {}".format(authCode))
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(authCode)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print ("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+    login_session['logged_in'] = True
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    flash("you are now logged in as %s" % login_session['username'])
+    return output
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
--- catalog/helpers.py
+++ catalog/helpers.py
@@ -1,6 +1,5 @@
-from flask import request
+import random
 
-def filterByCategory():
-    ''' will be using all items'''
-    select = request.form.get('categories')
-    return select
+def gen_random_string(valid, length):
+        random_string = ''.join(valid) # join together members of tuple which is iterable
+        return ''.join(random.choice(random_string) for x in range(length)) # returns a random string w/ length chars
